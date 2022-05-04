d14ed1655c53a4dcf2c15491c1141ca4e1c86759
LHL-source <97887598+LHL-source@users.noreply.github.com>
2022-05-04 17:10:44 +0200

    Add files via upload

--- H20.4AssignFilFinder_TestV2.0 _Part1_2_3/Javascript2.js
+++ H20.4AssignFilFinder_TestV2.0 _Part1_2_3/Javascript2.js
@@ -1,58 +1,57 @@
-/*Section of assignment: Tips and tricks
-Q4:get ALL movies on the console via function
-works ? yes
-Q4step1:get the <ul> by using the id that you made in your HTML structure
+/*-------------------------------------------------------------*/
 
-/*------------------------------------------------------------------*/
-/*Q4:start over put the .append of li ul in the 
-method.map in stead of in the method forEach 
-works ? yes excellent job*/
-/* change the title to the link by using tag <img>
-works ? yes*/
-/* par1 get ALL movies on the screen works? yes*/
-var movieUl = document.querySelector("#containerIdItems");
+getMovies(movies);
+/*Function: getMovies w? yes start part 1*/
+function getMovies() {
+    var movieUl = document.querySelector("#containerIdItems");
 
-const movieMap = movies.map((movies) => {
-    /* create li works ? yes*/
-    var newLi = document.createElement('li');
-    //console.log("newli: ", newLi); /*works ? yes*/
+    const movieMap = movies.map((movies) => {
+        /* create li works ? yes*/
+        var newLi = document.createElement('li');
+        //console.log("newli: ", newLi); /*works ? yes*/
 
-    /* create a <img> works ? yes */
-    var newImg1 = document.createElement('img');
-    //console.log('newImg1:', newImg1); /*works ?yes*/
+        /* create a <img> works ? yes */
+        var newImg1 = document.createElement('img');
+        //console.log('newImg1:', newImg1); /*works ?yes*/
 
-    /*append newImag1 to movieUl works? yes*/
-    var ulAppNewLi = movieUl.appendChild(newLi);
-    //console.log('ulAppNewLi;  ', ulAppNewLi); /*works? y*/
+        /*append newImag1 to movieUl works? yes*/
+        var ulAppNewLi = movieUl.appendChild(newLi);
+        //console.log('ulAppNewLi;  ', ulAppNewLi); /*works? y*/
 
-    /* append newImg1 to newLi works ? yes Q: append or appendChild ?*/
-    var newLiAppNewImg1 = newLi.appendChild(newImg1);
-    // console.log('newLiAppNewImg1', newLiAppNewImg1); /*works ? y*/
+        /* append newImg1 to newLi works ? yes Q: append or appendChild ?*/
+        var newLiAppNewImg1 = newLi.appendChild(newImg1);
+        // console.log('newLiAppNewImg1', newLiAppNewImg1); /*works ? y*/
 
-    /*get the link with property poster to the console works?yes excellent job let
-     let op de VOLGORDE is ZEER belangrijk eerst aan maken en stapsgewijs ul, li, img, src aan elkaar koppelen
-     want dit de Dom heeft een boom structuur */
-    newImg1.src = (movies.poster);
-    var newImg1Src = newImg1.src;
-    //console.log("newImg1.src: ", newImg1Src); /*works ? yes*/
+        /*get the link with property poster to the console works?yes excellent job let
+         let op de VOLGORDE is ZEER belangrijk eerst aan maken en stapsgewijs ul, li, img, src aan elkaar koppelen
+         want de Dom heeft een boom structuur */
+        newImg1.src = (movies.poster);
+        var newImg1Src = newImg1.src;
+        //console.log("newImg1.src: ", newImg1Src); /*works ? yes*/
 
-    return newLi
-        //console.log('return newLi:', newLi);
+        return newLi
+            //console.log('return newLi:', newLi);
 
-});
+    }); /*movxxMap*/
+} /*func getxxvies*/
 
-/*end part 1 works? yes*/
-/*-------------------------------------------------------------------*/
+/*end Function: getMovies  works? yes, end part 1*/
 /*-------------------------------------------------------------------*/
 
 
-/*part2b test(add 1 eventlisener to the ul and use e.target to get which
-    one of the 5 buttons has been clicke).works? yes.*/
-/*get the ul id with querySelector*/
+/*Start :Function: removMovies: W?Yes ---------------------------------------------------------------------*/
+function removeMovies() {
 
-//console.log('movieUl: ', movieUl); /*works ? y*/
+    var parentContUL1 = document.querySelector('#containerIdItems');
+    var childLi1 = document.querySelectorAll('#newMovie, #avengers, #men, #princess, #batman ');
+    parentContUL1.innerHTML = ""; /*works? */
+}
+/*End: Remove movies----------------------------------------------------------------------*/
 
-/*step 1: make the test switch with the button*/
+
+/*start Declaration of : querySelector(all)--------------------------------------------*/
+
+
 var movieItemsId = document.querySelector('#movieItemsId');
 //console.log('movieItemsId:  ', movieItemsId); /*works? y*/
 
@@ -61,10 +60,6 @@ var newMovieId = document.querySelector('#newMovie');
 
 var movieAllLi = document.querySelectorAll('#newMovie, #avengers, #men, #princess, #batman');
 //console.log("movieAllLi: ", movieAllLi); /*works? y*/
-/* change the nodelist into an array */
-//var movieAllLiArray = Array.from(movieAllLi);
-//console.log('movieAllLiArray', movieAllLiArray);
-/*works ? y*/
 
 var avengersId = document.querySelector('#avengers');
 //console.log("avengersId", avengersId);/* works? y*/
@@ -78,18 +73,18 @@ var princessId = document.querySelector('#princess');
 var batmanId = document.querySelector('#batman');
 //console.log("batmanId", batmanId); /* works? y*/
 
-/*make the eventlisener for the ul*/
-movieItemsId.addEventListener('change', (movieAllLi) => {
-    //alert(movieAllLi.target);
-    /*works ? y*/
-    console.log("Entering eventlistenerUl:  ", movieAllLi.target);
-    /*--------------------------------------------------------------------------------------*/
+/*end Declaration querySelector-------------------------------------------------------------*/
 
-    //console.log("eventlistenerUlMovieAllLiTargetValue:  ", movieAllLi.target.value);
-    /*works? yes where can you find it in the property in the devtool?*/
+
+/*Start EventListener and switch:make the eventlisener for the ul-----------------------------------------------*/
+
+movieItemsId.addEventListener('change', (movieAllLi) => {
+
     var targetValue = movieAllLi.target.value;
-    console.log('targetValue: ', targetValue);
-    /*works Yes*/
+    console.log('targetValue: ', targetValue); /*works ?*/
+
+    removeMovies();
+
     /*switch works ? yes good job en the dummy calling function : works ? yes*/
     switch (targetValue) {
         case 'newMovie':
@@ -104,6 +99,8 @@ movieItemsId.addEventListener('change', (movieAllLi) => {
             }); /*moxxfilter*/
 
             console.log('newMovie2', newMovie2);
+            getMovies(newMovie2);
+
 
             break;
 
@@ -112,6 +109,7 @@ movieItemsId.addEventListener('change', (movieAllLi) => {
                 return movie.title.toLowerCase().includes(targetValue);
             })
             console.log(' movie1', movie1);
+
             break;
 
         case 'men':
@@ -119,6 +117,7 @@ movieItemsId.addEventListener('change', (movieAllLi) => {
                 return movie.title.toLowerCase().includes(targetValue);
             })
             console.log(' movie1', movie1);
+
             break;
 
         case 'princess':
@@ -126,6 +125,7 @@ movieItemsId.addEventListener('change', (movieAllLi) => {
                 return movie.title.toLowerCase().includes(targetValue);
             })
             console.log(' movie1', movie1);
+
             break;
 
         case 'batman':
@@ -133,6 +133,7 @@ movieItemsId.addEventListener('change', (movieAllLi) => {
                 return movie.title.toLowerCase().includes(targetValue);
             })
             console.log(' movie1', movie1);
+
             break;
 
         default:
@@ -141,62 +142,4 @@ movieItemsId.addEventListener('change', (movieAllLi) => {
 
 }) /* both are part of movieItemsId*/
 
-/*-switch end--------------------------------------------------------------*/
-
-
-/*------------------------------------------------------------------*/
-/*noteblock test B: works ? y*/
-
-//var dummyTargetValue = 'newMovie';
-//console.log("newMovie1: ", dummyTargetValue); /*w?yes*/
-
-//var newMovie2 = movies.filter((moviesEachItem) => {
-//   var onlyMovie = moviesEachItem.type;
-
-
-// var moviesYearPars = parseInt(moviesEachItem.year);
-// console.log('moviesYearPars: ', moviesYearPars) /*w? yes? result: 25NAN*/
-
-
-//return moviesYearPars >= 2014
-
-//}); /*moxxfilter*/
-
-//console.log('newMovie2', newMovie2);
-
-/*noteblock test B: the end  */
-
-
-/*noteblock test B: test if else if else*/
-
-var dummyTargetValue = 'newMovie';
-console.log("newMovie1: ", dummyTargetValue); /*w?yes*/
-var newMovie2 = movies.filter((moviesEachItem) => {
-    var onlyMovie = moviesEachItem.type;
-    console.log('onlyMovie', onlyMovie); /*w? y*/
-    if (onlyMovie === "movie") {
-
-        var moviesYearPars = parseInt(moviesEachItem.year);
-
-        // console.log('moviesYearPars: ', moviesYearPars) /*w? yes? result: 25NAN*/
-
-        if (moviesYearPars === 2014) {
-            console.log('if1: ', moviesYearPars);
-        } else if (moviesYearPars > 2014) {
-
-            console.log('if2:', moviesYearPars);
-
-        } /*else if */
-        else {
-            console.log('else : ', moviesYearPars);
-        } /*else*/
-
-    }; /*if onlyMovie*/
-    //} /*if onlymx*/
-    return moviesYearPars
-
-}); /*moxxfilter*/
-
-console.log('newMovie2', newMovie2);
-
-/*noteblock test B: the end  */
+/*-switch en eventListener end--------------------------------------------------------------*/
